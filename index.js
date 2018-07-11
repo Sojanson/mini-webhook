46a44e2db5a2a09650b42c849eb6612e8e24b67a
Johann Yucra Navarrete <jyucra@biobiochile.cl>
2018-07-11 14:25:03 -0400

    getting message

--- index.js
+++ index.js
@@ -28,11 +28,12 @@ app.post('/webhook', (req, res) => {
 
     // Iterates over each entry - there may be multiple if batched
     body.entry.forEach(function(entry) {
-    	console.log(entry);
 
       // Gets the message. entry.messaging is an array, but 
       // will only ever contain one message, so we get index 0
       let webhook_event = entry.messaging[0];
+      console.log(webhook_event);
+      
     });
 
     // Returns a '200 OK' response to all requests
