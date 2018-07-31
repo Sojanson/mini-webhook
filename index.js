6feb89890bbbd43466657698f151f4646b4f6b17
Johann Yucra Navarrete <jyucra@biobiochile.cl>
2018-07-31 10:08:26 -0400

    from work

--- index.js
+++ index.js
@@ -226,7 +226,8 @@ function subscribeUser(user_psid, suscripcion) {
 }
 
 function getUserData(user_psid) {
-	
+
+	console.log(user_psid);	
 	request({
 		"uri": "https://graph.facebook.com/" + user_psid,
 		"method": "GET",
