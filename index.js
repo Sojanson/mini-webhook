7c414fbef38933ffdd329308ea7291aef4877c20
Johann Yucra Navarrete <jyucra@biobiochile.cl>
2018-08-02 08:26:04 -0400

    from work

--- index.js
+++ index.js
@@ -201,10 +201,10 @@ function postbackHandler(evento) {
 }
 function getCategory(slug) {
 	let sqlQuery = `SELECT id, name, slug FROM bot_categories WHERE slug = '${slug}'`;
-	conf.MYSQL.query(sqlQuery, function (err, result, fields){
+	conf.MYSQL.query(sqlQuery, (err, result, fields) =>{
 		if (err) throw err;
-		return {"id": result[0].id,	"name": result[0].name,	"slug": result[0].slug
-		};
+		let category = {"id": result[0].id,	"name": result[0].name,	"slug": result[0].slug};
+		return category;
 		
 	});
 }
