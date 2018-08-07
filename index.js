1f70a08c53d629107c3a7edc39218b1fddda8b63
Johann Yucra Navarrete <jyucra@biobiochile.cl>
2018-08-07 12:27:38 -0400

    revisando

--- index.js
+++ index.js
@@ -187,7 +187,7 @@ function messageHandler(evento) {
 						let notas = [];
 						for (let categoria of categorias) {
 							console.log(categoria);
-							notas.push(JSON.parse(posts[categoria.slug]));
+							notas.push(JSON.stringify(posts[categoria.slug]));
 						}
 
 						sendNewsMessage(sender, notas);
